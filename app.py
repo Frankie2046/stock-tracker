6fdbecc17b2bf3b91e6a5151e937eca0e4b73018
frank <francis-233@outlook.com>
2026-01-15 22:13:14 +0800

    change port

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def index():
     return render_template('index.html', stocks=stocks)
 
 if __name__ == '__main__':
-    app.run(debug=True,port = 5000)
+    app.run(host="0.0.0.0",debug=True,port = 5000)
 
